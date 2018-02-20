cf53f28821189aaebc11c5a072f2d0e4046dd046
MarthaCampos <martha.campos6@gmail.com>
2018-02-20 20:15:54 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { PesquisarComponent } from './pesquisar/pesquisar.component';
+import { AnunciarComponent } from './anunciar/anunciar.component';
+import { DestaquesComponent } from './destaques/destaques.component';
+import { NovidadesComponent } from './novidades/novidades.component';
+import { FooterComponent } from './footer/footer.component';
+import { ProdutosComponent } from './produtos/produtos.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HeaderComponent,
+    LoginComponent,
+    PesquisarComponent,
+    AnunciarComponent,
+    DestaquesComponent,
+    NovidadesComponent,
+    FooterComponent,
+    ProdutosComponent
   ],
   imports: [
     BrowserModule
